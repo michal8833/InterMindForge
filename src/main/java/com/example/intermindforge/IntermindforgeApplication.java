732aac071d4e6068dcbd8a736e3ac1771a719cfd
AleksanderPostrzednik <antaroupost@gmail.com>
2024-04-11 22:44:55 +0200

    Spring Intializr

---
+++ src/main/java/com/example/intermindforge/IntermindforgeApplication.java
@@ -0,0 +1,13 @@
+package com.example.intermindforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntermindforgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntermindforgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/intermindforge/IntermindforgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.intermindforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntermindforgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
